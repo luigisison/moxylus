85df38150db5c803ff1402c2bdbfdd4d78186b16
Luigi Sison <lsison@moxylus.com>
2016-03-01 10:39:50 -0800

    Update install-odoo9c.sh

--- Odoo9Community/install-odoo9c.sh
+++ Odoo9Community/install-odoo9c.sh
@@ -19,6 +19,23 @@
 # ./install-odoo9c.sh
 ################################################################################
 
+##fixed parameters
+#odoo
+OE_USER="odoo"
+OE_HOME="/$OE_USER"
+OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
+#The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
+#Set to true if you want to install it, false if you don't need it or have it already installed.
+INSTALL_WKHTMLTOPDF="True"
+#Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
+OE_PORT="8069"
+#Choose the Odoo version which you want to install. For example: 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
+#IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 9.0
+OE_VERSION="9.0"
+#set the superadmin password
+OE_SUPERADMIN="admin"
+OE_CONFIG="${OE_USER}-server"
+
 #--------------------------------------------------
 # Install ODOO
 #--------------------------------------------------
