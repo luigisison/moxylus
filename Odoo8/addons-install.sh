8f845a51b608d63b5ce93a27f370a128e41f7bcd
Luigi Sison <lsison@moxylus.com>
2016-03-02 11:11:37 -0800

    Create addons-install.sh

---
+++ Odoo8/addons-install.sh
@@ -0,0 +1,3 @@
+sudo git clone --depth 1 --branch 8.0 https://github.com/OCA/department.git /odoo/custom/addons
+sudo git clone --depth 1 --branch 8.0 https://github.com/odoomrp/odoomrp-wip.git /odoo/custom/addons
+sudo git clone --depth 1 --branch 8.0 https://github.com/odoomrp/odoomrp-utils.git odoo/custom/addons/odoomrp-utils
