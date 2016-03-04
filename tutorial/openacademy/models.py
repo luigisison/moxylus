c746f5ad1411cf0795978a9542eec85b2e0772c5
Luigi Sison <lsison@moxylus.com>
2016-03-03 16:15:30 -0800

    Update models.py

--- tutorial/openacademy/models.py
+++ tutorial/openacademy/models.py
@@ -8,6 +8,11 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    
+    <!-- A course has a responsible user. 
+    Translates into a Many2one relationship with Resource Users data model -->
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
 
 <!-- Define Session data model -->    
 class Session(models.Model):
@@ -17,6 +22,15 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    
+    <!-- A session has an instructor. 
+    Translates into a Many2one relationship with Resource Partner data model -->
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    
+    <!-- A session is related to a course. Conversely, a course has one or more sessions. 
+    Translates into a Many2one relationship with Course data model -->
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
     
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
