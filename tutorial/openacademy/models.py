c162473adf07da28d9d9040bae4f0302d6de58ca
Luigi Sison <lsison@moxylus.com>
2016-03-05 10:28:38 -0800

    Update models.py

--- tutorial/openacademy/models.py
+++ tutorial/openacademy/models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+<!-- Enable calendar view -->
+from datetime import timedelta
+
 from openerp import models, fields, api, exceptions
 
 <!-- Define Course data model -->
@@ -73,6 +76,8 @@ class Session(models.Model):
     
     <!-- Calculate number of taken seats --> 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+        compute='_get_end_date', inverse='_set_end_date')
 
     <!-- Calculate percentage of taken seats -->
     @api.depends('seats', 'attendee_ids')
@@ -100,6 +105,31 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+
+    <!-- Add calendar view to the Session model -->
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
 
     <!-- Check that the instructor is not an attendee in their own class -->
     @api.constrains('instructor_id', 'attendee_ids')
