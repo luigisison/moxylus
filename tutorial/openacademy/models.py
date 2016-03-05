b9e0e7e7f9d5ca2c2ff47ad08d3779fe34ae8b9c
Luigi Sison <lsison@moxylus.com>
2016-03-05 07:53:30 -0800

    Update models.py

--- tutorial/openacademy/models.py
+++ tutorial/openacademy/models.py
@@ -56,7 +56,25 @@ class Session(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
     
-    
+    <!-- Warn about invalid values for taken seats -->
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
+<!-- Model template -->
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
 
