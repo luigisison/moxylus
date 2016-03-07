8f684b27aa64eaa305211a05e9c3256c9b58cf37
Luigi Sison <lsison@moxylus.com>
2016-03-07 14:52:21 -0800

    Update __openerp__.py

--- tutorial/openacademy/__openerp__.py
+++ tutorial/openacademy/__openerp__.py
@@ -31,6 +31,7 @@
         'views/openacademy.xml'
         'views/partner.xml',
         'views/session_workflow.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
